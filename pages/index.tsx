283cc8b54d3570c98fde9e999bc6435fad4456cf
ZeFIRrr <wladyslaw.siniajew@simfactor.pl>
2024-01-26 22:09:03 +0100

    dsa

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,10 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count i`)}>
             Show Alert
         </button>
+      </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -24,7 +25,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image 
+            <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
